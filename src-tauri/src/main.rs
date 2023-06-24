7a4006f06240b3df10be0d0e21f4e088f10a8e04
Arunava Ghosh <102473837+its-ag@users.noreply.github.com>
2023-06-24 18:37:04 +0530

    feat:add multithreading

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,9 +1,9 @@
-// this hides the console for Windows release builds
 #![cfg_attr(
     all(not(debug_assertions), target_os = "windows"),
     windows_subsystem = "windows"
 )]
 
+use std::thread;
 use tauri_plugin_window_state;
 use tauri::Manager;
 
@@ -12,7 +12,9 @@ fn main() {
     .plugin(tauri_plugin_window_state::Builder::default().build())
     .setup(|app| {
         let window = app.get_window("main").unwrap();
-        window.eval("window.location.replace('https://pw.live/');").unwrap();
+        thread::spawn(move || {
+            window.eval("window.location.replace('https://pw.live/');").unwrap();
+        });
         Ok(())
     })
     .run(tauri::generate_context!())
